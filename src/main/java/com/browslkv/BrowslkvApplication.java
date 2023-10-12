1583f44729e280ef559ebc48d8e6636db159d533
VL <lusakvm@gmail.com>
2023-10-12 23:56:18 +0300

    Project init

---
+++ src/main/java/com/browslkv/BrowslkvApplication.java
@@ -0,0 +1,13 @@
+package com.browslkv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrowslkvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrowslkvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/browslkv/BrowslkvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.browslkv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrowslkvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
